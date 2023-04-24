4e890d91f9445b4daedc17a30402773917239f0f
Jacob Damasco <damasco@usc.edu>
2023-04-23 21:25:59 -0700

    added doodler & some collision detection

--- main_controller.v
+++ main_controller.v
@@ -22,6 +22,14 @@ module main_controller(
 	reg [9:0] px3_pos, py3_pos;
 	reg [9:0] px4_pos, py4_pos;
 	reg [9:0] px5_pos, py5_pos;
+
+	// rng
+	reg [9:0] seed;
+	reg [9:0] rand_num;
+
+	// collision detection
+	reg collision;
+	reg block_red;
 	
 	/************************************************/ 
 	/* COLORS 										*/
@@ -35,54 +43,135 @@ module main_controller(
 	parameter PASTEL_BLUE = 12'b0110_1000_1111; // 68F
 	parameter WHITE = 12'b1111_1111_1111;
 
-	wire[11:0] rgb_doodler_data;
-
-	doodler_rom doodler_rom_data(.clk(clk), .row(row), .col(col), .color_data(rgb_doodler_data));
 
 	/************************************************/ 
 	/* CHARACTER POSITIONING 						*/
 	/************************************************/
-	assign char_fill = 	(vCount >= cy_pos) && 
-						(vCount <= cy_pos+50) && 
-						(hCount >= cx_pos-15) && 
-						(hCount <= cx_pos+15);
-	
+	assign char_fill = doodler_feet || doodler_vert || doodler_hor;
+	// (vCount >= cy_pos) && (vCount <= cy_pos+64) && (hCount >= cx_pos) && (hCount <= cx_pos+64);
+
+	assign doodler_feet = ((hCount >= cx_pos+10) && (hCount <= cx_pos+11) && (vCount >= cy_pos+57) && (vCount <= cy_pos+61)) || 
+	((hCount >= cx_pos+18) && (hCount <= cx_pos+19) && (vCount >= cy_pos+55) && (vCount <= cy_pos+62)) ||
+	((hCount >= cx_pos+27) && (hCount <= cx_pos+28) && (vCount >= cy_pos+55) && (vCount <= cy_pos+62)) ||
+	((hCount >= cx_pos+35) && (hCount <= cx_pos+36) && (vCount >= cy_pos+54) && (vCount <= cy_pos+59)) ||
+	((hCount >= cx_pos+28) && (hCount <= cx_pos+31) && (vCount >= cy_pos+62) && (vCount <= cy_pos+63)) ||
+	((hCount >= cx_pos+37) && (hCount <= cx_pos+42) && (vCount >= cy_pos+60) && (vCount <= cy_pos+61)) ||
+	((hCount >= cx_pos+20) && (hCount <= cx_pos+23) && (vCount >= cy_pos+63) && (vCount <= cy_pos+64)) ||
+	((hCount >= cx_pos+12) && (hCount <= cx_pos+15) && (vCount >= cy_pos+63) && (vCount <= cy_pos+64)) ||
+	((hCount == cx_pos+11) && (vCount >= cy_pos+62) && (vCount <= cy_pos+63)) ||
+	((hCount == cx_pos+19) && (vCount == cy_pos+63)) ||
+	((hCount == cx_pos+36) && (vCount == cy_pos+60));
+
+	assign doodler_vert = ((hCount == cx_pos+6) && (vCount >= cy_pos+37) && (vCount <= cy_pos+54)) ||
+	((hCount == cx_pos+4) && (vCount >= cy_pos+21) && (vCount <= cy_pos+30)) ||
+	((hCount == cx_pos+5) && (vCount >= cy_pos+17) && (vCount <= cy_pos+21)) ||
+	((hCount == cx_pos+40) && (vCount >= cy_pos+33) && (vCount <= cy_pos+53)) ||
+	((hCount == cx_pos+6) && (vCount >= cy_pos+14) && (vCount <= cy_pos+17)) ||
+	((hCount == cx_pos+6) && (vCount >= cy_pos+37) && (vCount <= cy_pos+54)) ||
+	((hCount == cx_pos+7) && (vCount >= cy_pos+12) && (vCount <= cy_pos+14)) ||
+	((hCount == cx_pos+8) && (vCount >= cy_pos+11) && (vCount <= cy_pos+12)) ||
+	((hCount == cx_pos+9) && (vCount >= cy_pos+10) && (vCount <= cy_pos+11)) ||
+	((hCount == cx_pos+10) && (vCount >= cy_pos+9) && (vCount <= cy_pos+10)) ||
+	((hCount == cx_pos+11) && (vCount >= cy_pos+7) && (vCount <= cy_pos+8)) ||
+	((hCount == cx_pos+12) && (vCount == cy_pos+7)) ||
+	((hCount == cx_pos+13) && (vCount == cy_pos+6)) ||
+	((hCount == cx_pos+40) && (vCount >= cy_pos+30) && (vCount <= cy_pos+37)) ||
+	((hCount == cx_pos+40) && (vCount >= cy_pos+16) && (vCount <= cy_pos+18)) ||
+	((hCount == cx_pos+42) && (vCount >= cy_pos+20) && (vCount <= cy_pos+21)) ||
+	((hCount == cx_pos+43) && (vCount >= cy_pos+21) && (vCount <= cy_pos+22)) ||
+	((hCount == cx_pos+44) && (vCount >= cy_pos+22) && (vCount <= cy_pos+23)) ||
+	((hCount == cx_pos+56) && (vCount >= cy_pos+24) && (vCount <= cy_pos+30)) ||
+	((hCount == cx_pos+61) && (vCount >= cy_pos+24) && (vCount <= cy_pos+32)) ||
+	((hCount == cx_pos+30) && (vCount >= cy_pos+20) && (vCount <= cy_pos+21)) ||
+	((hCount == cx_pos+31) && (vCount >= cy_pos+19) && (vCount <= cy_pos+22)) ||
+	((hCount == cx_pos+35) && (vCount >= cy_pos+20) && (vCount <= cy_pos+21)) ||
+	((hCount == cx_pos+36) && (vCount >= cy_pos+19) && (vCount <= cy_pos+22)) ||
+	((hCount == cx_pos+41) && (vCount >= cy_pos+32) && (vCount <= cy_pos+33)) ||
+	((hCount == cx_pos+42) && (vCount >= cy_pos+31) && (vCount <= cy_pos+32)) ||
+	((hCount == cx_pos+60) && (vCount >= cy_pos+22) && (vCount <= cy_pos+23)) ||
+	((hCount == cx_pos+4) && (vCount >= cy_pos+37) && (vCount <= cy_pos+38));
+
+	assign doodler_hor = ((hCount >= cx_pos+14) && (hCount <= cx_pos+15) && (vCount == cy_pos+5)) ||
+	((hCount >= cx_pos+15) && (hCount <= cx_pos+16) && (vCount == cy_pos+4)) ||
+	((hCount >= cx_pos+16) && (hCount <= cx_pos+18) && (vCount == cy_pos+3)) ||
+	((hCount >= cx_pos+18) && (hCount <= cx_pos+27) && (vCount == cy_pos+2)) ||
+	((hCount >= cx_pos+18) && (hCount <= cx_pos+27) && (vCount == cy_pos+2)) ||
+	((hCount >= cx_pos+28) && (hCount <= cx_pos+30) && (vCount == cy_pos+3)) ||
+	((hCount >= cx_pos+30) && (hCount <= cx_pos+31) && (vCount == cy_pos+4)) ||
+	((hCount >= cx_pos+31) && (hCount <= cx_pos+32) && (vCount == cy_pos+5)) ||
+	((hCount >= cx_pos+32) && (hCount <= cx_pos+33) && (vCount == cy_pos+6)) ||
+	((hCount >= cx_pos+33) && (hCount <= cx_pos+34) && (vCount == cy_pos+7)) ||
+	((hCount >= cx_pos+34) && (hCount <= cx_pos+35) && (vCount == cy_pos+8)) ||
+	((hCount >= cx_pos+35) && (hCount <= cx_pos+36) && (vCount == cy_pos+9)) ||
+	((hCount >= cx_pos+36) && (hCount <= cx_pos+37) && (vCount == cy_pos+10)) ||
+	((hCount == cx_pos+37) && (vCount == cy_pos+10)) ||
+	((hCount >= cx_pos+37) && (hCount <= cx_pos+38) && (vCount == cy_pos+12)) ||
+	((hCount >= cx_pos+38) && (hCount <= cx_pos+39) && (vCount == cy_pos+13)) ||
+	((hCount == cx_pos+39) && (vCount == cy_pos+14)) ||
+	((hCount >= cx_pos+39) && (hCount <= cx_pos+40) && (vCount == cy_pos+15)) ||
+	((hCount == cx_pos+41) && (vCount == cy_pos+18)) ||
+	((hCount >= cx_pos+41) && (hCount <= cx_pos+42) && (vCount == cy_pos+19)) ||
+	((hCount >= cx_pos+45) && (hCount <= cx_pos+47) && (vCount == cy_pos+23)) ||
+	((hCount >= cx_pos+47) && (hCount <= cx_pos+53) && (vCount == cy_pos+24)) ||
+	((hCount >= cx_pos+53) && (hCount <= cx_pos+56) && (vCount == cy_pos+23)) ||
+	((hCount >= cx_pos+44) && (hCount <= cx_pos+52) && (vCount == cy_pos+30)) ||
+	((hCount >= cx_pos+53) && (hCount <= cx_pos+55) && (vCount == cy_pos+31)) ||
+	((hCount >= cx_pos+59) && (hCount <= cx_pos+60) && (vCount == cy_pos+33)) ||
+	((hCount >= cx_pos+57) && (hCount <= cx_pos+59) && (vCount == cy_pos+21)) ||
+	((hCount >= cx_pos+7) && (hCount <= cx_pos+39) && (vCount == cy_pos+37)) ||
+	((hCount >= cx_pos+7) && (hCount <= cx_pos+39) && (vCount == cy_pos+44)) ||
+	((hCount >= cx_pos+7) && (hCount <= cx_pos+39) && (vCount == cy_pos+50)) ||
+	((hCount >= cx_pos+28) && (hCount <= cx_pos+39) && (vCount == cy_pos+53)) ||
+	((hCount >= cx_pos+15) && (hCount <= cx_pos+28) && (vCount == cy_pos+54)) ||
+	((hCount >= cx_pos+9) && (hCount <= cx_pos+15) && (vCount == cy_pos+55)) ||
+	((hCount >= cx_pos+7) && (hCount <= cx_pos+9) && (vCount == cy_pos+54)) ||
+	((hCount == cx_pos+5) && (vCount == cy_pos+39)) ||
+	((hCount == cx_pos+43) && (vCount == cy_pos+31)) ||
+	((hCount == cx_pos+57) && (vCount == cy_pos+22)) ||
+	((hCount == cx_pos+57) && (vCount == cy_pos+31)) ||
+	((hCount == cx_pos+58) && (vCount == cy_pos+32));
+
+
 	/************************************************/ 
 	/* PLATFORM POSITIONING 						*/
 	/************************************************/
 	assign platform1_fill = (vCount >= py1_pos) &&
-							(vCount <= py1_pos+20) &&
+							(vCount <= py1_pos+15) &&
 							(hCount >= px1_pos-50) &&
 							(hCount <= px1_pos+50);
 
 	assign platform2_fill = (vCount >= py2_pos) &&
-							(vCount <= py2_pos+20) &&
+							(vCount <= py2_pos+15) &&
 							(hCount >= px2_pos-50) &&
 							(hCount <= px2_pos+50);
 
 	assign platform3_fill = (vCount >= py3_pos) &&
-							(vCount <= py3_pos+20) &&
+							(vCount <= py3_pos+15) &&
 							(hCount >= px3_pos-50) &&
 							(hCount <= px3_pos+50);
 
 	assign platform4_fill = (vCount >= py4_pos) &&
-							(vCount <= py4_pos+20) &&
+							(vCount <= py4_pos+15) &&
 							(hCount >= px4_pos-50) &&
 							(hCount <= px4_pos+50);
 
 	assign platform5_fill = (vCount >= py5_pos) &&
-							(vCount <= py5_pos+20) &&
+							(vCount <= py5_pos+15) &&
 							(hCount >= px5_pos-50) &&
 							(hCount <= px5_pos+50);
 
+
 	/* when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
 	will output some data to every pixel and not just the images you are trying to display */
+	/************************************************/ 
+	/* OUTPUTTING RGB LOGIC 						*/
+	/************************************************/
 	always@ (*) 
 	begin : fill_blocks
 		if(~bright )	//force black if not inside the display area
 			rgb = 12'b0000_0000_0000;
 		else if (char_fill)
-			rgb = PURPLE;
+			rgb = PURPLE;	
     	else if (platform1_fill || platform2_fill || platform3_fill) 
             rgb = GREEN;
         else if (platform4_fill || platform5_fill) 
@@ -92,6 +181,9 @@ module main_controller(
 	end
 
 
+	/************************************************/ 
+	/* COUNTERS 									*/
+	/************************************************/
 	always@(posedge clk, posedge rst)
 	begin : jump_counter
 		if (rst) begin
@@ -99,26 +191,72 @@ module main_controller(
 		end
 		else if (clk) begin
 			jump_ctr <= jump_ctr + 1'b1;
-			if (jump_ctr == 128) begin
+			if (jump_ctr == 128 || collision == 1) begin
 				jump_ctr <= 0;
 			end
 		end
 	end
 	
+
+	always @(posedge clk) 
+	begin : rng
+		if (rst) begin
+			seed <= 0;
+			rand_num <= 0;
+		end else begin
+			seed <= seed + 1; // Increment seed for next iteration
+			rand_num <= (seed * 437) % 541 + 190; // Generate random number between 190 and 730
+			if (rand_num < 190)
+				rand_num <= rand_num + 190;
+			else if (rand_num > 730)
+				rand_num <= rand_num - 100;
+		end
+	end
+
+
+	/************************************************/ 
+	/* CHAR MOVEMENT		 						*/
+	/************************************************/
 	always@(posedge clk, posedge rst) 
-	begin : char_mvmt_controls
+	begin : char_mvmt
 		// starting position of char
 		if(rst) begin 
 			cx_pos <= 464;
-			cy_pos <= 464;
+			cy_pos <= 500;
+			collision <= 0;
 		end
 		else if (clk) begin
 			// controlling the automatic jumping
-			if (jump_ctr < 64) begin
+			if (jump_ctr < 64 && collision == 0) begin
 				cy_pos <= cy_pos - 2;
 			end
-			else if (jump_ctr > 64) begin
+			else if (jump_ctr > 64 && collision == 0) begin
 				cy_pos <= cy_pos + 2;
+				// check for collision on the way down
+				if ((cx_pos+32 <= (px1_pos + 50)) && (cx_pos+32 >= (px1_pos - 50)) && (cy_pos+64 == py1_pos + 1))
+					collision <= 1;
+				else if ((cx_pos+32 <= (px2_pos + 50)) && (cx_pos+32 >= (px2_pos - 50)) && (cy_pos+64 == py2_pos + 1))
+					collision <= 1;
+				else if ((cx_pos+32 <= (px3_pos + 50)) && (cx_pos+32 >= (px3_pos - 50)) && (cy_pos+64 == py3_pos + 1))
+					collision <= 1;
+				else if ((cx_pos+32 <= (px4_pos + 50)) && (cx_pos+32 >= (px4_pos - 50)) && (cy_pos+64 == py4_pos + 1))
+					collision <= 1;
+				else if ((cx_pos+32 <= (px5_pos + 50)) && (cx_pos+32 >= (px5_pos - 50)) && (cy_pos+64 == py5_pos + 1))
+					collision <= 1;
+			end
+
+			if (collision) begin
+				if ((cx_pos+32 <= (px1_pos + 50)) && (cx_pos+32 >= (px1_pos - 50)) && (cy_pos+64 == py1_pos + 1))
+					cy_pos <= py1_pos - 64 + 1;
+				else if ((cx_pos+32 <= (px2_pos + 50)) && (cx_pos+32 >= (px2_pos - 50)) && (cy_pos+64 == py2_pos + 1))
+					cy_pos <= py2_pos - 64 + 1;
+				else if ((cx_pos+32 <= (px3_pos + 50)) && (cx_pos+32 >= (px3_pos - 50)) && (cy_pos+64 == py3_pos + 1))
+					cy_pos <= py3_pos - 64 + 1;
+				else if ((cx_pos+32 <= (px4_pos + 50)) && (cx_pos+32 >= (px4_pos - 50)) && (cy_pos+64 == py4_pos + 1))
+					cy_pos <= py4_pos - 64 + 1;
+				else if ((cx_pos+32 <= (px5_pos + 50)) && (cx_pos+32 >= (px5_pos - 50)) && (cy_pos+64 == py5_pos + 1))
+					cy_pos <= py5_pos - 64 + 1;
+				collision <= 0; 
 			end
 
 			// controls
@@ -135,12 +273,13 @@ module main_controller(
 		end
 	end
 
+
 	always@(posedge clk, posedge rst) 
-	begin : platform_positioning
+	begin : platform_mvmt
 		// starting position of char
 		if(rst) begin 
 			px1_pos <= 197;
-			py1_pos <= 56;
+			py1_pos <= 100;
 
             px2_pos <= 624;
             py2_pos <= 312;
@@ -153,6 +292,37 @@ module main_controller(
 
             px5_pos <= 217;
             py5_pos <= 466;
+		end
+		else if (clk) begin
+			// if a collision is detected
+			if (collision) begin
+				py1_pos <= py1_pos + 10;
+				py2_pos <= py2_pos + 10;
+				py3_pos <= py3_pos + 10;
+				py4_pos <= py4_pos + 10;
+				py5_pos <= py5_pos + 10;
+			end
+    
+			if (py1_pos > 516) begin
+				py1_pos <= 50;
+				px1_pos <= rand_num; 
+			end 
+			if (py2_pos > 516) begin
+				py2_pos <= 50;
+				px2_pos <= rand_num; 
+			end 
+			if (py3_pos > 516) begin
+				py3_pos <= 50;
+				px3_pos <= rand_num; 
+			end 
+			if (py4_pos > 516) begin
+				py4_pos <= 50;
+				px4_pos <= rand_num; 
+			end 
+			if (py5_pos > 516) begin
+				py5_pos <= 50;
+				px5_pos <= rand_num; 
+			end
 		end
 	end
 
